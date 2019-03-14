a4562c83337ed64eec5390bb2098a81bf7329bbc
Muhammad Achdiat Fata <muhammadahdiat@gmail.com>
2019-03-14 14:26:05 +0700

    uploadpertama

---
+++ frontend/views/mahasiswa/index.php
@@ -0,0 +1,23 @@
+<?php
+/* @var $this yii\web\View */
+?>
+<h2>Daftar Mahasiswa SI</h2>
+<ol>
+	<li>Ayu Amalia</li>
+	<li>Farah Shofiyah</li>
+	<li>Farras Syafira</li>
+	<li>Jennie Imelia Fitri Sundari</li>
+	<li>Karina Sukmawati</li>
+	<li>Muhammad Achdiat Fata</li>
+	<li>Noviyati Nur Wulan</li>
+	<li>Rahmawati Nur Laela</li>
+	<li>Rahmat Panji Ramadhan</li>
+	<li>Robi'ah Qoyyimah</li>
+	<li>Sallima Hayati</li>
+	<li>Silmi Rizki Ramadhani</li>
+	<li>Tanalina Rahma Ilahiyah</li>
+</ol>
+
+<?php  
+
+?>
